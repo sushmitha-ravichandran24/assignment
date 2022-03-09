1c05a9b9cbf9dae87727e3c7e8ee4499d4a6b226
sushmitha-ravichandran24 <57221187+sushmitha-ravichandran24@users.noreply.github.com>
2022-03-08 21:42:46 -0800

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package program2;
+
+
+public class datatypes{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    byte biscuitPrice = 10;
+	        byte lays = 50;
+	        int ice = 50; //space storage
+	        short i = 50;
+	        int chocoBar = 200;
+	        short iceCrea = 90;
+	        int number = 128;// byte means, size exceeding
+//	        datatype variablename assignment operator value
+//	        System.out.println("128");
+//	        System.out.println(number); // know diff b/w "
+	        long number2 = 30L;
+	        long number23 = 40L; //no duplicate variable
+	        float a = 4.088f;
+	        double b = 23.89898989;
+	        char data = 'a'; //single quotes
+	        char Data = 'a'; //case sensitive
+//	        char char = 'd';   // don't ue keywords. ? fixed for particular purpose
+	        int d =10;  //initialization
+	        int e = 80;  // declaration
+	        System.out.println("java");
+	        System.out.println("program");
+	        System.out.print("java ");
+	        System.out.print("program");
+	        System.out.println();
+//	        boolean baloon = true;
+
+	}
+
+}
