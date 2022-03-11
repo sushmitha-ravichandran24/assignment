0d19ed0ae4560eaf927b1718a605be44aeb906bf
sushmitha-ravichandran24 <57221187+sushmitha-ravichandran24@users.noreply.github.com>
2022-03-10 20:27:20 -0800

    Add files via upload

---
+++ datatypesdemo.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class datatypesdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 200;
+		{
+			System.out.println(a);
+		}
+		byte b = 20;
+		{
+			System.out.println(b);
+	}
+
+}
